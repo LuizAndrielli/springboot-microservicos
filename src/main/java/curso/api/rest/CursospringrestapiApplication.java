61e4d8d411f2b766eecfc462c2f0f9a265c7b59e
Luiz Augusto <gutoandrielli@gmail.com>
2024-04-03 15:52:41 -0300

    Configurando projeto Spring

--- src/main/java/curso/api/rest/CursospringrestapiApplication.java
+++ src/main/java/curso/api/rest/CursospringrestapiApplication.java
@@ -2,7 +2,13 @@ package curso.api.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan(basePackages = {"curso.api.rest.model"})
+@ComponentScan(basePackages = {"curso.*"})
+@EnableJpaRepositories(basePackages = {"curso.api.rest.repository"})
 @SpringBootApplication
 public class CursospringrestapiApplication {
 
